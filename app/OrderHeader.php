5249167173ab48b2ec6867dd64da423ae6a65146
susindusri <susindusri.venusit@gmail.com>
2019-11-13 00:20:58 +0530

    added relation

--- app/OrderHeader.php
+++ app/OrderHeader.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\OrderList;
 
 class orderHeader extends Model
 {
+    public function OrderList() {
 
+        return $this->hasMany(OrderList);
+    }
 }
--- app/OrderList.php
+++ app/OrderList.php
@@ -4,11 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Item;
+use App\orderHeader;
 
 class OrderList extends Model
 {
     public function items() {
         return $this->hasMany(Item);
+    }
+
+    public function orderHeader() {
+
+        return $this->belongsTo(orderHeader);
     }
 
 }
